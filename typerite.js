25dc5fa71b2ed0d7047ea0d7e67200d686c4c52f
jol-tm <joaolucastmagalhaes@gmail.com>
2024-12-02 11:39:46 -0300

    Adicionada algumas funções

---
+++ typerite.js
@@ -0,0 +1,11 @@
+function typewrite(txt, tgt, speed) {
+    let i = 0;
+    const interval = setInterval(() => {
+        if (i < txt.length) {
+            tgt.innerHTML += txt[i];
+            i++;
+        } else {
+            clearInterval(interval);
+        }
+    }, speed);
+}
